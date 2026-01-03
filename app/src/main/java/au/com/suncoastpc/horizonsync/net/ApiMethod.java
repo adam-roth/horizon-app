626854359fd448f7ea23d63a071f2796acb980ec
Adam Roth <aroth.bigtribe@gmail.com>
2026-01-04 00:59:08 +1000

    remove expired API key

--- app/src/main/java/au/com/suncoastpc/horizonsync/net/ApiMethod.java
+++ app/src/main/java/au/com/suncoastpc/horizonsync/net/ApiMethod.java
@@ -1,7 +1,7 @@
 package au.com.suncoastpc.horizonsync.net;
 
 public enum ApiMethod {
-	LOAD_DATA("/data-gateway/api/loadData"),			        //format=json&apiKey=e4335a64660e40b1826ab61296bb0a26&params=where=1%3D1&f=pjson&context=Society/Society_SCRC/MapServer/17
+	LOAD_DATA("/data-gateway/api/loadData"),			        //format=json&apiKey=YOUR_API_KEY&params=where=1%3D1&f=pjson&context=Society/Society_SCRC/MapServer/17
 	PING("/data-gateway/sync/ping"),                            //<no params required>
 	GET_UPDATES("/data-gateway/sync/getUpdates");			    //timestamp=0&clientTime=0&clientLatency=0
 
